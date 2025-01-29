930ebfc1e4f4ffec9079e503b28eb1169a39c138
Tirumanisetti Srinu <srinu.t22@iiits.in>
2025-01-29 11:39:30 +0530

    used custom middleware

---
+++ index4.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const port = 3000;
+var bandname = "";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+function bandNameGenerator(req, res, next) {
+  console.log(req.body);
+  bandname = req.body["email"] +" "+ req.body["name"];
+  next();
+}
+
+app.post('/submit', bandNameGenerator, (req, res) => {
+  console.log(req.body);
+  res.send(`<h1>Your Gmail and name are: ${bandname}</h1>`);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);  // Display the server's listening port on the console.
+});
